2ec79edc86b8c066fc85f6a7c95bc9933a860678
HongHyunsoo <cuoxote@gmail.com>
2025-11-11 15:16:15 +0900

    [Feat] Trail Shader 작성 완료

---
+++ Effect_Project/Assets/Scripts/EnemyDetector.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    [SerializeField] private float detectionRadius = 10f;
+    [SerializeField] private LayerMask enemyLayer;
+
+    public GameObject GetClosestiEnemy()
+    {
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, detectionRadius, enemyLayer);
+
+        if(enemiesInRange.Length > 0)
+        {
+            GameObject bestTarget = null;
+            float closestDistanceSqr = Mathf.Infinity;  //최대 숫자로시작해서 가까운 거리로 계속 전환된다.
+            Vector3 currentPosition = transform.position;
+
+            foreach(Collider enemyCollider in enemiesInRange)
+            {
+                //자기 자신은 제외
+                if(enemyCollider.gameObject == this.gameObject)
+                    continue;
+
+                Vector3 directionToTarget = enemyCollider.transform.position - currentPosition;
+                float dSqrToTarget = directionToTarget.sqrMagnitude;
+
+                if (dSqrToTarget < closestDistanceSqr)
+                {
+                    closestDistanceSqr = dSqrToTarget;
+                    bestTarget = enemyCollider.gameObject;
+                }
+            }
+            return bestTarget;
+            
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public List<GameObject> GetEnemiesInRange()
+    {
+        List<GameObject> enemiesList = new List<GameObject>();
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, detectionRadius, enemyLayer) ;
+
+        foreach(Collider enemyCollider in enemiesInRange)
+        {
+            if (enemyCollider.gameObject != this.gameObject)
+            {
+                enemiesList.Add(enemyCollider.gameObject);
+            }
+        }
+
+        return enemiesList;
+    }
+
+    private void OnDrawGizmosSelected() //에디터에서 범위 확인용
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
